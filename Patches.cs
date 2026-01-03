0e2993924a330e57c122e13d57696811f84889a3
Marcy <87911841+DemonBunnyBon@users.noreply.github.com>
2026-01-03 14:18:30 +0100

    1.7.0

--- Patches.cs
+++ Patches.cs
@@ -7,6 +7,7 @@ using Il2CppTLD.Gear;
 using Il2Cpp;
 using Il2CppTLD.IntBackedUnit;
 using Il2CppTLD.AI;
+using UnityEngine.AddressableAssets;
 
 
 namespace PlantHarvestTweaker
@@ -14,52 +15,52 @@ namespace PlantHarvestTweaker
     internal static class Patches
     {
 
-        [HarmonyPatch(typeof(Harvestable), "Harvest")]
+        [HarmonyPatch(typeof(Harvestable), nameof(Harvestable.Harvest))]
         public class HarvestToolPatch
         {
             public static void Postfix(ref Harvestable __instance)
             {
                 GearItem tool = __instance.GetRequiredTool();
-                if(tool != null)
+                if (tool != null)
                 {
                     tool.Degrade(Settings.instance.ToolDegrade);
 
                 }
-                
+
             }
         }
 
-        [HarmonyPatch(typeof(Harvestable), "Harvest")]
+        [HarmonyPatch(typeof(Harvestable), nameof(Harvestable.Harvest))]
         public class ZeroItemsPatch
         {
             public static void Prefix(ref Harvestable __instance)
             {
-                if(Settings.instance.CanFail == true)
+                if (Settings.instance.CanFail == true)
                 {
                     float failChance = (Settings.instance.FailChance + (0.75f * __instance.m_GearItemCountMax) + (0.25f * __instance.m_GearItemCountMin));
-                    if(Utils.RollChance(failChance))
+                    if (Utils.RollChance(failChance))
                     {
                         __instance.m_Harvested = true;
                         MeshRenderer primaryMesh = __instance.gameObject.GetComponent<MeshRenderer>();
-                        if(primaryMesh != null)
+                        if (primaryMesh != null)
                         {
                             primaryMesh.enabled = false;
                         }
                         Transform cattails = __instance.gameObject.transform.FindChild("OBJ_CatTailShrubFlowers");
-                        
+
                         if (cattails != null)
                         {
-                           MeshRenderer cattailsMesh = cattails.GetComponent<MeshRenderer>();
-                           if (cattailsMesh != null)
-                           {
+                            MeshRenderer cattailsMesh = cattails.GetComponent<MeshRenderer>();
+                            if (cattailsMesh != null)
+                            {
                                 cattailsMesh.enabled = false;
-                           }
+                            }
                         }
-                        if((__instance.gameObject.name.ToLowerInvariant().Contains("preharvest") || __instance.gameObject.name.ToLowerInvariant().Contains("sapling") || __instance.gameObject.name.ToLowerInvariant().Contains("burdock")) && __instance.m_ActivateObjectPostHarvest != null)
+                        if ((__instance.gameObject.name.ToLowerInvariant().Contains("preharvest") || __instance.gameObject.name.ToLowerInvariant().Contains("sapling") || __instance.gameObject.name.ToLowerInvariant().Contains("burdock")) && __instance.m_ActivateObjectPostHarvest != null)
                         {
                             __instance.m_ActivateObjectPostHarvest.active = true;
                         }
-                        if(__instance.gameObject.GetComponent<Collider>() != null)
+                        if (__instance.gameObject.GetComponent<Collider>() != null)
                         {
                             __instance.gameObject.GetComponent<Collider>().enabled = false;
                         }
@@ -71,39 +72,42 @@ namespace PlantHarvestTweaker
             }
         }
 
-        [HarmonyPatch(typeof(Harvestable), "AddAlternateTools")]
+        [HarmonyPatch(typeof(Harvestable), nameof(Harvestable.InitializeRequiredTools))]
         public class ToolPatch
         {
-            public static void Postfix(ref Harvestable __instance)
+            public static void Prefix(ref Harvestable __instance)
             {
                 GameObject[] NoToolsArray = [];
                 List<GameObject> fulllist = new List<GameObject>();
-                GameObject[] KnifeArray = [GearItem.LoadGearItemPrefab("GEAR_Knife").gameObject, GearItem.LoadGearItemPrefab("GEAR_KnifeImprovised").gameObject];
+                GameObject[] KnifeArray = [Addressables.LoadAssetAsync<GameObject>("GEAR_Knife").WaitForCompletion(), Addressables.LoadAssetAsync<GameObject>("GEAR_KnifeImprovised").WaitForCompletion()];
                 GameObject[] DLCKnifeArray = [];
                 GameObject[] CTKnifeArray = [];
-                if(GearItem.LoadGearItemPrefab("GEAR_JeremiahKnife") != null)
+                if (GearItem.LoadGearItemPrefab("GEAR_JeremiahKnife") != null)
                 {
-                    CTKnifeArray = [GearItem.LoadGearItemPrefab("GEAR_JeremiahKnife").gameObject, GearItem.LoadGearItemPrefab("GEAR_KnifeScrapMetal").gameObject];
+                    CTKnifeArray = [Addressables.LoadAssetAsync<GameObject>("GEAR_JeremiahKnife").WaitForCompletion(), Addressables.LoadAssetAsync<GameObject>("GEAR_KnifeScrapMetal").WaitForCompletion()];
                     fulllist.AddRange(CTKnifeArray);
                 }
-                if(GearItem.LoadGearItemPrefab("GEAR_CougarClawKnife") != null)
+                if (Addressables.LoadAssetAsync<GameObject>("GEAR_CougarClawKnife").WaitForCompletion() != null)
                 {
-                    DLCKnifeArray = [GearItem.LoadGearItemPrefab("GEAR_SurvivalKnife").gameObject, GearItem.LoadGearItemPrefab("GEAR_CougarClawKnife").gameObject];
+                    DLCKnifeArray = [Addressables.LoadAssetAsync<GameObject>("GEAR_SurvivalKnife").WaitForCompletion(), Addressables.LoadAssetAsync<GameObject>("GEAR_CougarClawKnife").WaitForCompletion()];
                     fulllist.AddRange(KnifeArray);
                     fulllist.AddRange(DLCKnifeArray);
-                    
+
                 }
                 KnifeArray = fulllist.ToArray();
-                GameObject[] HatchetArray = [GearItem.LoadGearItemPrefab("GEAR_Hatchet").gameObject, GearItem.LoadGearItemPrefab("GEAR_HatchetImprovised").gameObject, GearItem.LoadGearItemPrefab("GEAR_Hacksaw").gameObject];
+                GameObject[] HatchetArray = [Addressables.LoadAssetAsync<GameObject>("GEAR_Hatchet").WaitForCompletion(), Addressables.LoadAssetAsync<GameObject>("GEAR_HatchetImprovised").WaitForCompletion(), Addressables.LoadAssetAsync<GameObject>("GEAR_Hacksaw").WaitForCompletion()];
                 GameObject[] ExtraArray = [GearItem.LoadGearItemPrefab("GEAR_Prybar").gameObject];
                 GameObject[] ToolArray = [];
                 fulllist = new List<GameObject>();
                 fulllist.AddRange(HatchetArray);
                 fulllist.AddRange(KnifeArray);
                 fulllist.AddRange(ExtraArray);
-                ToolArray = fulllist.ToArray(); 
+                ToolArray = fulllist.ToArray();
 
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_Rosehip"))
+                string gearName = __instance.m_GearPrefab?.name ?? "";
+                string secondGearName = __instance.m_SecondGearPrefab?.name ?? "";
+
+                if (gearName == "GEAR_RoseHip")
                 {
                     switch (Settings.instance.rosehiptool)
                     {
@@ -124,7 +128,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_ReishiMushroom"))
+                if (gearName == "GEAR_ReishiMushroom")
                 {
                     switch (Settings.instance.reishitool)
                     {
@@ -145,7 +149,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_OldMansBeardHarvested"))
+                if (gearName == "GEAR_OldMansBeardHarvested")
                 {
                     switch (Settings.instance.lichentool)
                     {
@@ -166,7 +170,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_CattailStalk") || __instance.m_SecondGearPrefab == GearItem.LoadGearItemPrefab("GEAR_CattailTinder"))
+                if (gearName == "GEAR_CattailStalk" || secondGearName == "GEAR_CattailTinder")
                 {
                     switch (Settings.instance.cattailtool)
                     {
@@ -187,7 +191,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_Burdock"))
+                if (gearName == "GEAR_Burdock")
                 {
                     switch (Settings.instance.burdocktool)
                     {
@@ -208,7 +212,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_Acorn"))
+                if (gearName == "GEAR_Acorn")
                 {
                     switch (Settings.instance.acorntool)
                     {
@@ -229,7 +233,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_BirchSapling"))
+                if (gearName == "GEAR_BirchSapling")
                 {
                     switch (Settings.instance.birchtool)
                     {
@@ -250,7 +254,7 @@ namespace PlantHarvestTweaker
                             break;
                     }
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_MapleSapling"))
+                if (gearName == "GEAR_MapleSapling")
                 {
                     switch (Settings.instance.mapletool)
                     {
@@ -275,50 +279,53 @@ namespace PlantHarvestTweaker
         }
 
 
-        [HarmonyPatch(typeof(Harvestable), "Awake")]
+        [HarmonyPatch(typeof(Harvestable), nameof(Harvestable.Awake))]
 
         public class HarvestablePatch
         {
             public static void Postfix(ref Harvestable __instance)
             {
-                if(__instance.m_GearPrefab==GearItem.LoadGearItemPrefab("GEAR_Rosehip"))
+                string gearName = __instance.m_GearPrefab?.name ?? "";
+                string secondGearName = __instance.m_SecondGearPrefab?.name ?? "";
+
+                if (gearName == "GEAR_RoseHip")
                 {
                     __instance.m_GearItemCountMin = Settings.instance.RoseMin;
                     __instance.m_GearItemCountMax = Settings.instance.RoseMax;
                 }
-                if(__instance.m_GearPrefab==GearItem.LoadGearItemPrefab("GEAR_ReishiMushroom"))
+                if (gearName == "GEAR_ReishiMushroom")
                 {
                     __instance.m_GearItemCountMin = Settings.instance.ReishiMin;
                     __instance.m_GearItemCountMax = Settings.instance.ReishiMax;
                 }
-                if(__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_OldMansBeardHarvested"))
+                if (gearName == "GEAR_OldMansBeardHarvested")
                 {
                     __instance.m_GearItemCountMin = Settings.instance.LichenMin;
                     __instance.m_GearItemCountMax = Settings.instance.LichenMax;
                 }
-                if(__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_CattailStalk") || __instance.m_SecondGearPrefab == GearItem.LoadGearItemPrefab("GEAR_CattailTinder"))
+                if (gearName == "GEAR_CattailStalk" || secondGearName == "GEAR_CattailTinder")
                 {
                     __instance.m_GearItemCountMax = Settings.instance.CatTailStalkMax;
                     __instance.m_GearItemCountMin = Settings.instance.CatTailStalkMin;
                     __instance.m_SecondGearItemCountMin = Settings.instance.CatTailHeadMin;
                     __instance.m_SecondGearItemCountMax = Settings.instance.CatTailHeadMax;
                 }
-                if(__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_Burdock"))
+                if (gearName == "GEAR_Burdock")
                 {
                     __instance.m_GearItemCountMax = Settings.instance.BurdockMax;
                     __instance.m_GearItemCountMin = Settings.instance.BurdockMin;
                 }
-                if(__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_Acorn"))
+                if (gearName == "GEAR_Acorn")
                 {
                     __instance.m_GearItemCountMax = Settings.instance.AcornMax;
                     __instance.m_GearItemCountMin = Settings.instance.AcornMin;
                 }
-                if(__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_BirchSapling"))
+                if (gearName == "GEAR_BirchSapling")
                 {
                     __instance.m_GearItemCountMax = Settings.instance.BirchMax;
                     __instance.m_GearItemCountMin = Settings.instance.BirchMin;
                 }
-                if (__instance.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_MapleSapling"))
+                if (gearName == "GEAR_MapleSapling")
                 {
                     __instance.m_GearItemCountMax = Settings.instance.MapleMax;
                     __instance.m_GearItemCountMin = Settings.instance.MapleMin;
@@ -327,51 +334,50 @@ namespace PlantHarvestTweaker
         }
 
 
-        [HarmonyPatch(typeof(HarvestableInteraction), "Awake")]
+        [HarmonyPatch(typeof(HarvestableInteraction), nameof(Harvestable.Awake))]
 
         public class HarvestableInteractionPatch
         {
             public static void Postfix(ref HarvestableInteraction __instance)
             {
-                if(__instance.m_Harvestable.m_GearPrefab==GearItem.LoadGearItemPrefab("Gear_Rosehip"))
+                string gearName = __instance.m_Harvestable.m_GearPrefab?.name ?? "";
+                string secondGearName = __instance.m_Harvestable.m_SecondGearPrefab?.name ?? "";
+
+                if (gearName == "GEAR_RoseHip")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.RoseHarvest;
                 }
-                if(__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("Gear_ReishiMushroom"))
+                if (gearName == "GEAR_ReishiMushroom")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.ReishiHarvest;
                 }
-                if(__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("Gear_OldMansBeardHarvested"))
+                if (gearName == "GEAR_OldMansBeardHarvested")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.LichenHarvest;
                 }
-                if(__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_CattailStalk") || __instance.m_Harvestable.m_SecondGearPrefab == GearItem.LoadGearItemPrefab("GEAR_CattailTinder"))
+                if (gearName == "GEAR_CattailStalk" || secondGearName == "GEAR_CattailTinder")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.CatTailHarvest;
                 }
-                if(__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("GEAR_Burdock"))
+                if (gearName == "GEAR_Burdock")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.BurdockHarvest;
                 }
-                if(__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("Gear_Acorn"))
+                if (gearName == "GEAR_Acorn")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.AcornHarvest;
                 }
-                if (__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("Gear_BirchSapling"))
+                if (gearName == "GEAR_BirchSapling")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.BirchHarvest;
                 }
-                if (__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("Gear_MapleSapling"))
+                if (gearName == "GEAR_MapleSapling")
                 {
                     __instance.m_DefaultHoldTime = Settings.instance.MapleHarvest;
                 }
-                if (GearItem.LoadGearItemPrefab("GEAR_Salt") != null)
+                if (gearName == "GEAR_Salt")
                 {
-                    if (__instance.m_Harvestable.m_GearPrefab == GearItem.LoadGearItemPrefab("Gear_Salt"))
-                    {
-                        __instance.m_DefaultHoldTime = Settings.instance.SaltHarvest;
-                    }
-                    
+                    __instance.m_DefaultHoldTime = Settings.instance.SaltHarvest;
                 }
             }
         }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Marcy")]
 
 
-[assembly: AssemblyVersion("1.6.9")]
-[assembly: AssemblyFileVersion("1.6.9")]
-[assembly: MelonInfo(typeof(PlantHarvestTweakerMod), "PlantHarvestTweaker", "1.6.9", "Marcy", null)]
+[assembly: AssemblyVersion("1.7.0")]
+[assembly: AssemblyFileVersion("1.7.0")]
+[assembly: MelonInfo(typeof(PlantHarvestTweakerMod), "PlantHarvestTweaker", "1.7.0", "Marcy", null)]
 
 [assembly: MelonGame("Hinterland", "TheLongDark")]
